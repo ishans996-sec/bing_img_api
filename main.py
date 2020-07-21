bc031bfc260645c9b1eeb0402e35a318bf6fffc0
ishans996-sec <developer.ishans996@gmail.com>
2020-07-21 15:40:14 +0530

    error handling

--- main.py
+++ main.py
@@ -20,26 +20,30 @@ def bing_image_search(query):
     query= query.split()
     query='+'.join(query)
     url="http://www.bing.com/images/search?q=" + query + "&FORM=HDRSC2"
-
-    #add the directory for your image here
-    DIR="Pictures"
     header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
-    soup = get_soup(url,header)
-    image_result_raw = soup.find("a",{"class":"iusc"})
+    try:
+        soup = get_soup(url,header)
+        image_result_raw = soup.find("a",{"class":"iusc"})
 
-    m = json.loads(image_result_raw["m"])
-    murl, turl = m["murl"],m["turl"]# mobile image, desktop image
+        m = json.loads(image_result_raw["m"])
+        murl, turl = m["murl"],m["turl"]
 
-    image_name = urllib.parse.urlsplit(murl).path.split("/")[-1]
-    return (image_name,murl, turl)
+        image_name = urllib.parse.urlsplit(murl).path.split("/")[-1]
+        return (image_name,murl, turl)
+
+    except:
+        return None
+        
 
 @app.get("/get_url/")
 async def root(q: str = ""):
-    # print('req_received')
-    # print("q_str =", q)
+    if(q == ''):    return {"status":"ERROR: EMPTY QUERY"}
     results = bing_image_search(q)
-    # print(results)
-    if len(results) >=1 :
-        return {"status":"SUCCESS","url": results[1]}
+    if not results or len(results) <= 1:
+        return {"status" : "FAILURE", "url": "NOT_FOUND"}
     else:
-        return {"status" : "FAILURE"}
+        return {"status":"SUCCESS","url": results[1]}
+    # if len(results) > 1:
+        
+    
+        
